6aac1141344496ac7fd2a94696e5102f04f62a36
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-25 22:57:59 -0400

    Add model import statements do debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -2,6 +2,9 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.Fight import Fight
+from models.Fighter import Fighter
+from models.Weight_class import Weight_class
 import ipdb
 
 
