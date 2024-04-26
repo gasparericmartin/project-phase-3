c522b5c6420a81c15f17d7f73030eba27a0189fa
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-25 22:06:40 -0400

    Create weight and name properties

--- lib/models/Weight_class.py
+++ lib/models/Weight_class.py
@@ -7,7 +7,22 @@ class Weight_class:
         self.id = id
     
     def __repr__(self):
-        print(f'Class weight limit: {self.weight}' + \
+        print(f'Class weight limit: {self.weight}lbs' + \
               f'Class name: {self.name}' + \
                  f'Class id: {self.id}')
-        
+
+    @property
+    def weight(self):
+        return self._weight
+    
+    @weight.setter
+    def weight(self, weight):
+        self._weight = weight
+    
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
