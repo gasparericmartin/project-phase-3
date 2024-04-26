917932593fac8a61a27ce0e5b2adaa39ab43d3e5
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-25 22:26:14 -0400

    Create Fight class, init, and repr

---
+++ lib/models/Fight.py
@@ -0,0 +1,23 @@
+class Fight:
+
+    def __init__(
+            self, 
+            date, 
+            ftr_1 = None, 
+            ftr_2 = None, 
+            winner = None, 
+            id = None):
+        self.date = date
+        self.ftr_1 = ftr_1
+        self.ftr_2 = ftr_2
+        self.winner = winner
+        self.id = id
+    
+    def __repr__(self):
+        print(
+            f'Fight date: {self.date}' + \
+            f'Fighter 1: {self.ftr_1}' + \
+            f'Fighter 2: {self.ftr_2}' + \
+            f'Winner: {self.winner}' + \
+            f'id: {self.id}'
+        )
