586057e8563dfb7d5e37c1b2b033ea0eb7ccec7e
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-05-13 09:43:49 -0400

    Change seed data

--- lib/seed.py
+++ lib/seed.py
@@ -14,18 +14,22 @@ def main():
     Fighter.create_table()
     Fight.create_table()
 
-    Weight_class.create(125, "flyweight")
-    Weight_class.create(135, "bantamweight")
-    Weight_class.create(145, "featherweight")
+    Weight_class.create(125, "Flyweight")
+    Weight_class.create(135, "Bantamweight")
+    Weight_class.create(145, "Featherweight")
 
     Fighter.create("Mighty Mouse", 35, 1, 20, 5)
     Fighter.create("Assassin Baby", 32, 1, 15, 3)
+    Fighter.create("Aljamain Sterling", 29, 2, 20, 5)
+    Fighter.create("Henry Cejudo", 31, 2, 16, 3)
     Fighter.create("Max Holloway", 32, 3, 30, 0)
     Fighter.create("Jose Aldo", 40, 3, 35, 7)
 
     Fight.create("02/28/2024", 1, 2, 1)
     Fight.create("09/09/2022", 3, 4, 3)
     Fight.create("09/09/2022", 1, 2, 2)
+    Fight.create("04/27/2007", 3, 4, 3)
+
 
 if __name__ == "__main__":
     main()
